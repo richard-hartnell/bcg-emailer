09ee4560691397f1640bc9aa2140fdfa5061c3d1
Richard Hartnell <109207464+richard-hartnell@users.noreply.github.com>
2024-10-06 09:28:16 -0700

    Final version w00t

--- script.py
+++ script.py
@@ -1,41 +1,41 @@
 import smtplib
+from datetime import date, timedelta
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-# you don't need to point to the meeting I guess
+next_week = date.today() + timedelta(days=7)
+next_week_formatted = next_week.strftime("%B %d")
+three_days = date.today() + timedelta(days=3)
+three_days_formatted = three_days.strftime("%B %d")
+if date.today().weekday() == 6:
+    meeting_date = next_week_formatted
+    reminder = f"""Hi all,
 
-meeting_date = "Sunday, January 43rd" # prompt('What is the date of the meeting?')
-
-# write the reminder email for 1 week
-
-week_reminder = '''
-Hi all,
-
-Here's the week-ahead reminder of our next Circus Guild meeting on ${date}.
+Here's the week-ahead reminder of our next Circus Guild meeting on {meeting_date}.
 
 Per usual, if you have any agenda items to add you can do so in the description field of the Google Calendar event. If you don't want a proposal subject to a ten-day waiting period presuming it passes this Sunday, then please submit it to the group in writing by this upcoming Thursday.
 
 Also per usual, if you need to attend the event virtually just let us know.
 
 RH
-'''
+"""
+if date.today().weekday() == 3:
+    meeting_date = three_days_formatted
+    reminder = f"""Dear BCG,
 
-# write the reminder email for 3 days
-
-days_reminder = '''
-Dear BCG,
-
-Here is your additional reminder for the upcoming Guild meeting on ${meeting_date} at 5 PM.
+Here is your additional reminder for the upcoming Guild meeting on {meeting_date} at 5 PM.
 
 If you intend to pass any proposals without a ten-day waiting period, please submit them to the Guild by writing by end of day today.
-'''
+"""
 
-# email to bcg
+with open('sender_email.txt', 'r') as file:
+    email_address = file.read().strip()
 
-# Your email and password
-email_address = "richard@bellinghamcircusguild.com"
 with open('email_password.txt', 'r') as file:
     email_password = file.read().strip()
+
+with open('target_email.txt', 'r') as file:
+    target_email = file.read().strip()
 
 # Email setup
 def send_email(recipient_email, subject, body):
@@ -46,7 +46,7 @@ def send_email(recipient_email, subject, body):
     msg['Subject'] = subject
     
     # Attach the email body
-    msg.attach(MIMEText(body, 'plain'))
+    msg.attach(MIMEText(body.strip(), 'plain'))
     
     try:
         # Connect to the Dreamhost SMTP server
@@ -59,4 +59,5 @@ def send_email(recipient_email, subject, body):
         print(f"Error sending email: {e}")
 
 # Example usage
-send_email("rfreemanh@gmail.com", "Test Subject", week_reminder)
+# email_subject = "BCG meeting email reminder: " + meeting_date
+send_email(target_email, ("BCG meeting email reminder: " + meeting_date), reminder)
